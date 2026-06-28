d0b07abc1f8e9417af173e6bd5fea97594f69c9f
Jason Peng <144397187+jasonpeng7@users.noreply.github.com>
2026-06-28 14:29:30 -0700

    Task #76224: Added env-var based host/port/auth
    Signed-off-by: Jason Peng <144397187+jasonpeng7@users.noreply.github.com>

--- tests/performance/conftest.py
+++ tests/performance/conftest.py
@@ -1,13 +1,20 @@
-import pytest 
-from pyhive import hive 
+import os
+import pytest
+from pyhive import hive
+
+_HOST = os.environ.get("SPARK_THRIFT_HOST", "localhost")
+_PORT = int(os.environ.get("SPARK_THRIFT_PORT", 10000))
+_USER = os.environ.get("SPARK_THRIFT_USER", "dbt")
+_AUTH = os.environ.get("SPARK_THRIFT_AUTH", "NOSASL")
+
 
 @pytest.fixture(scope="session")
 def thrift_connection():
     conn = hive.Connection(
-        host="spark_db",
-        port=10000,
-        username="dbt",
-        auth="NOSASL"
+        host=_HOST,
+        port=_PORT,
+        username=_USER,
+        auth=_AUTH,
     )
     yield conn
     conn.close()
